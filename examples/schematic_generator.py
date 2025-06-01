3e16e5fbeb9f491a225b5ec539a7f9efacb8f002
Ribqah <178738766+ribqahisabsent@users.noreply.github.com>
2025-06-01 01:59:10 -0400

    Make schematic generator script (renamed `script_app.py`)

---
+++ examples/schematic_generator.py
@@ -0,0 +1,17 @@
+import mc_diag_boat as db
+
+
+origin_x = int(input("origin x: "))
+origin_z = int(input("origin z: "))
+destination_x = int(input("destination x: "))
+destination_z = int(input("destination z: "))
+
+origin = db.Vec2(origin_x, origin_z)
+destination = db.Vec2(destination_x, destination_z)
+offset = destination - origin
+
+schem_name = db.name_schematic(origin, destination)
+schem = db.generate_schematic(offset, gap_size=1)
+schem.save(schem_name + ".litematica")
+print("Saved schematic", schem_name)
+
