b900051c06fb92724c13169d1650c8ceb3459bee
Ribqah <178738766+ribqahisabsent@users.noreply.github.com>
2025-06-01 02:03:41 -0400

    Add user control of gap size in `schematic_generator.py`

--- examples/schematic_generator.py
+++ examples/schematic_generator.py
@@ -5,13 +5,14 @@ origin_x = int(input("origin x: "))
 origin_z = int(input("origin z: "))
 destination_x = int(input("destination x: "))
 destination_z = int(input("destination z: "))
+gap_size = int(input("gap size: "))
 
 origin = db.Vec2(origin_x, origin_z)
 destination = db.Vec2(destination_x, destination_z)
 offset = destination - origin
 
 schem_name = db.name_schematic(origin, destination)
-schem = db.generate_schematic(offset, gap_size=1)
+schem = db.generate_schematic(offset, gap_size)
 schem.save(schem_name + ".litematica")
 print("Saved schematic", schem_name)
 
