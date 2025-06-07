2321d7d5c21a6fec425b27ae2bdfbebdfb66f169
Ribqah <178738766+ribqahisabsent@users.noreply.github.com>
2025-06-07 01:06:05 -0400

    Add `BoatAngle` import to `__init__.py`

--- mc_diag_boat/__init__.py
+++ mc_diag_boat/__init__.py
@@ -1,10 +1,12 @@
 from .schematic import name_schematic, generate_schematic
 from .vec2 import Vec2
+from .boat_angle import BoatAngle
 
 
 __all__ = [
     "name_schematic",
     "generate_schematic",
     "Vec2",
+    "BoatAngle",
 ]
 
