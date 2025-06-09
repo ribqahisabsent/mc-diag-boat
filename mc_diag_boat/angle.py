3bb2c336bebf4f7a1b119482e90bce6fd62e4587
Ribqah <178738766+ribqahisabsent@users.noreply.github.com>
2025-06-08 20:10:25 -0400

    Refactor `Angle` (back) to a subclass of `float`

--- mc_diag_boat/angle.py
+++ mc_diag_boat/angle.py
@@ -1,12 +1,8 @@
 from typing import ClassVar, Self, overload
-from dataclasses import dataclass
 from math import radians, sin
 
 
-@dataclass(frozen=True, slots=True)
-class Angle:
-    degrees: float
-
+class Angle(float):
     BOAT_ANGLE_STEP: ClassVar["Angle"]
     NORTH: ClassVar["Angle"]
     WEST: ClassVar["Angle"]
@@ -15,130 +11,20 @@ class Angle:
     ZERO: ClassVar["Angle"]
     BOAT_ANGLES: ClassVar[list["Angle"]]
 
-    def __post_init__(self):
-        normalized = (self.degrees + 180) % 360 - 180
-        object.__setattr__(self, "degrees", normalized)
-
-    def __add__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((self.degrees + other + 180) % 360 - 180)
-
-    __radd__ = __add__
-
-    def __sub__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((self.degrees - other + 180) % 360 - 180)
-
-    def __rsub__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((other - self.degrees + 180) % 360 - 180)
-
-    def __mul__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((self.degrees * other + 180) % 360 - 180)
-
-    __rmul__ = __mul__
-
-    def __truediv__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((self.degrees / other + 180) % 360 - 180)
-
-    def __rtruediv__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((other / self.degrees + 180) % 360 - 180)
-
-    def __floordiv__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((self.degrees // other + 180) % 360 - 180)
-
-    def __rfloordiv__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((other // self.degrees + 180) % 360 - 180)
-
-    def __mod__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((self.degrees % other + 180) % 360 - 180)
-
-    def __rmod__(self, other: "float | Angle") -> Self:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return type(self)((other % self.degrees + 180) % 360 - 180)
+    def __new__(cls, degrees: float) -> Self:
+        return super().__new__(cls, (degrees + 180) % 360 - 180)
 
     def __pos__(self) -> Self:
-        return type(self)(+self.degrees)
+        return type(self)(+self)
 
     def __neg__(self) -> Self:
-        return type(self)(-self.degrees)
+        return type(self)(-self)
 
-    def __abs__(self) -> Self:
-        return type(self)(abs(self.degrees))
+    def angular_dist(self, other: float) -> "Angle":
+        return Angle((other - self + 180) % 360 - 180)
 
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return self.degrees == other
-
-    def __ne__(self, other: object) -> bool:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return self.degrees != other
-
-    def __lt__(self, other: "float | Angle") -> bool:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return self.degrees < other
-
-    def __le__(self, other: "float | Angle") -> bool:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return self.degrees <= other
-
-    def __gt__(self, other: "float | Angle") -> bool:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return self.degrees > other
-
-    def __ge__(self, other: "float | Angle") -> bool:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return self.degrees >= other
-
-    def __int__(self) -> int:
-        return int(self.degrees)
-
-    def __float__(self) -> float:
-        return float(self.degrees)
-
-    def __bool__(self) -> bool:
-        return bool(self.degrees)
-
-    def __str__(self) -> str:
-        return str(self.degrees)
-
-    def __repr__(self) -> str:
-        return self.degrees.__repr__()
-
-    def __round__(self, n: int) -> Self:
-        return type(self)(round(self.degrees, n))
-
-    def angular_dist(self, other: "float | Angle") -> "Angle":
-        if isinstance(other, Angle):
-            other = other.degrees
-        return Angle((other - self.degrees + 180) % 360 - 180)
-
-    def unit_deviation(self, other: "float | Angle") -> float:
-        if isinstance(other, Angle):
-            other = other.degrees
-        return 2 * sin(radians(abs(self.degrees - other) / 2))
+    def unit_deviation(self, other: float) -> float:
+        return 2 * sin(radians(abs(self - other) / 2))
 
     @overload
     def closest_boat_angle(self) -> "Angle": ...
@@ -162,9 +48,9 @@ class Angle:
         if boat_angle == 180.0 or boat_angle == -180.0:
             return None
         if boat_angle < 0.0:
-            return boat_angle - self.BOAT_ANGLE_STEP, self
+            return Angle(boat_angle - self.BOAT_ANGLE_STEP), self
         if boat_angle > 0.0:
-            return self, boat_angle + self.BOAT_ANGLE_STEP
+            return self, Angle(boat_angle + self.BOAT_ANGLE_STEP)
         return -self.BOAT_ANGLE_STEP, self.BOAT_ANGLE_STEP
 
 
@@ -173,5 +59,5 @@ Angle.NORTH = Angle(-180.0)
 Angle.WEST = Angle(90.0)
 Angle.SOUTH = Angle(0.0)
 Angle.EAST = Angle(-90.0)
-Angle.BOAT_ANGLES = [index * Angle.BOAT_ANGLE_STEP for index in range(-128, 128)]
+Angle.BOAT_ANGLES = [Angle(index * Angle.BOAT_ANGLE_STEP) for index in range(-128, 128)]
 
