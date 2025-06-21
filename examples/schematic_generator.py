32a96a3a4145387019057e1aece8800d809670aa
Ribqah <178738766+ribqahisabsent@users.noreply.github.com>
2025-06-21 00:26:27 -0400

    Use `loop_input()` in `schematic_generator.py`

--- examples/schematic_generator.py
+++ examples/schematic_generator.py
@@ -20,19 +20,13 @@ Offset: {offset}
 Angle: {offset.angle()}
 Closest boat angles and offsets:""")
 for index, angle in closest_boat_angles.items():
-    print(f"    {index} : [offset: {angle[1]} , error: {offset - angle[1]} , angle: {angle[0]}]")
+    print(f"    {index} : [offset: {angle[1]} , error: {angle[1] - offset} , angle: {angle[0]}]")
 
-while True:
-    try:
-        choice = input("\nEnter index of desired boat angle (default 0): ")
-        if choice == "":
-            choice = 0
-        else:
-            choice = int(choice)
-        chosen_angle = closest_boat_angles[choice]
-        break
-    except Exception as e:
-        print(type(e).__name__, e)
+chosen_angle = closest_boat_angles[db.loop_input(
+    "\nEnter index of desired boat angle (default 0): ",
+    {index for index in closest_boat_angles.keys()},
+    default=0,
+)]
 
 schem_name = db.name_schematic(origin, origin + chosen_angle[1])
 schem = db.generate_schematic(chosen_angle[1], gap_size)
@@ -41,6 +35,5 @@ print("Saved schematic", schem_name)
 
 print(f"""
 Boat placement angle range: {chosen_angle[0].boat_placement_range()}
-    F3 angle while in boat: {round(chosen_angle[0], 1):.1f}
-""")
+    F3 angle while in boat: {round(chosen_angle[0], 1):.1f}""")
 
